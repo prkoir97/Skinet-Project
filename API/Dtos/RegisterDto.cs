46b7e2bbbcf747438b09fd685af20b932815276e
Priya <kpriy4@gmail.com>
2024-02-27 03:57:44 -0500

    End of Section 17

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -12,7 +12,7 @@ namespace API.Dtos
         public string Email { get; set; }
 
         [Required]
-        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+        [RegularExpression("(?=^.{6,15}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
         ErrorMessage = "Password must have 1 Uppercase letter, 1 Lowercase letter, 1 number, 1 non alphanumeric and at least 6 characters.")]
         public string Password { get; set; }
     }
